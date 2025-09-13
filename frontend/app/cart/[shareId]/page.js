200bb9fc0ccfb4182802abc312fab14305779664
Lucas Egea <lucasegea@gmail.com>
2025-09-13 08:02:51 -0600

    test de mejora cache images y demas

--- frontend/app/cart/[shareId]/page.js
+++ frontend/app/cart/[shareId]/page.js
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic'
 import HomePage from '@/app/page'
+
+export const dynamic = 'force-dynamic'
 
 export default function CartSharePage() {
   // Reutilizamos la Home para que /cart/:id muestre la misma UI
